9d2f17d02531c613871cdf2f4dba1fc86e20c41d
Hensel Jahja <henseljahja@gmail.com>
2022-06-05 18:28:52 +0700

    fix best time go

--- 121_best_time_to_buy_and_sell_stock/121_best_time_to_buy_and_sell_stock.go
+++ 121_best_time_to_buy_and_sell_stock/121_best_time_to_buy_and_sell_stock.go
@@ -1,18 +1,21 @@
+package besttimetobuyandsellstock
+
 import "math"
+
 func maxProfit(prices []int) int {
-    lowestPrice := math.MaxInt
-    todayProfit := 0
-    maxProfit := 0
-    
-    for i := 0; i < len(prices); i++{
-        if lowestPrice > prices[i] {
-            lowestPrice = prices[i]
-        }
-        todayProfit = prices[i] - lowestPrice
-        
-        if todayProfit > maxProfit {
-            maxProfit = todayProfit
-        }
-    }
-    return maxProfit
+	lowestPrice := math.MaxInt
+	todayProfit := 0
+	maxProfit := 0
+
+	for i := 0; i < len(prices); i++ {
+		if lowestPrice > prices[i] {
+			lowestPrice = prices[i]
+		}
+		todayProfit = prices[i] - lowestPrice
+
+		if todayProfit > maxProfit {
+			maxProfit = todayProfit
+		}
+	}
+	return maxProfit
 }
