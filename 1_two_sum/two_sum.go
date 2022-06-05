d81bbf16b25736f22332082a6147f570478a4914
Hensel Jahja <henseljahja@gmail.com>
2022-06-05 18:28:42 +0700

    fix two sum on go

--- 1_two_sum/two_sum.go
+++ 1_two_sum/two_sum.go
@@ -1,14 +1,16 @@
+package twosum
+
 func twoSum(nums []int, target int) []int {
-    hashmap := map[int]int{}
-    // Target = A + B
-    // Target - A = B
-    for index, A := range(nums){
-        B := target - A
-        _, present := hashmap[B]
-        if present{
-            return []int{hashmap[B],index}
-        }
-        hashmap[A] = index
-    }
-    return nil
+	hashmap := map[int]int{}
+	// Target = A + B
+	// Target - A = B
+	for index, A := range nums {
+		B := target - A
+		_, present := hashmap[B]
+		if present {
+			return []int{hashmap[B], index}
+		}
+		hashmap[A] = index
+	}
+	return nil
 }
